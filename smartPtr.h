76788fc6b47cfe61f19a5a734238ea5078764c44
xotnet <150250186+xotnet@users.noreply.github.com>
2025-11-22 17:35:05 +0000

    Create smartPtr.h

---
+++ smartPtr.h
@@ -0,0 +1,26 @@
+template<typename T>
+struct smartPtr {
+    private:
+    void memset (void* p, char o, unsigned int size) {
+        for (int i = 0; i<size; ++i) {
+            ((char*)p)[i] = o;
+        }
+    }
+    public:
+    T* ptr = NULL;
+    unsigned int size = 0;
+    smartPtr(unsigned int sz) {
+        ptr = new T[sz];
+        size = sz;
+    }
+    ~smartPtr() {
+        delete[] ptr;
+    }
+    T* get() {
+        return (T*)ptr;
+    }
+    void erase() {
+        unsigned int sizeInBytes = size * sizeof(T);
+        memset(ptr, 0, sizeInBytes);
+    }
+};
