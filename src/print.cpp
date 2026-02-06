2567be213191a95cf6b082976ff61da6560cd1d6
xotnet <150250186+xotnet@users.noreply.github.com>
2026-02-06 10:38:32 +0000

    Create print.cpp

---
+++ src/print.cpp
@@ -0,0 +1,12 @@
+#ifndef printX
+#define printX
+
+#include <iostream>
+
+// usage - print("Hello ", 25, " Its a Str", 0,0002f)
+template <typename... T>
+void print(T... args) {
+    ((std::cout << args), ...);
+}
+
+#endif
